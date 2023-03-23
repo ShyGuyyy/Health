d96d03b7a574f209465563258d44ae306a26205c
ShyGuyyy <kaspupavel@gmail.com>
2023-03-23 09:24:24 +0200

    commit

---
+++ Äpp.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import *
+
+win = tk.Tk()
+
+# Äppi geomeetriline suurus
+win.geometry("800x400")
+
+# Äppi nimi
+name = tk.Label(text="Health!")
+name.pack()
+
+# Excecutable
+win.mainloop()
+
+
+
