2e11e67e13d21db11f9f02ce97667026b0e84049
ShyGuyyy <kaspupavel@gmail.com>
2023-03-24 08:35:51 +0200

    commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from BMICalc import BMI
 import tkinter as tk
 from tkinter import *
+from tkinter import ttk
 
 # Funktsioon mis kutsub BMI klassi ja entrid kokku et kalkuleerida BMI.
 def bmi_calc(kaal_entry, pikkus_entry, result_lable):
@@ -13,17 +14,26 @@ def bmi_calc(kaal_entry, pikkus_entry, result_lable):
 # Tkinter akna avamine
 win = tk.Tk()
 
+# Äppi nimi
+win.title("Health!")
+
 # Äppi geomeetriline suurus
 win.geometry("800x400")
 
-# Äppi nimi
-name = tk.Label(text="Health!")
-name.pack()
+# Kaalu textbox loomine(m)
+kaal_info = tk.Label(text="Palun sisestage oma pikkus meetrites!")
+kaal_info.pack(side="left")
+kaal_info.place(x=50, y=75)
 
 # Kaalu (Entry) loomine
 kaal = Entry(master=win, font=("Comicsans", 23))
 kaal.pack(side="left")
 kaal.place(x=50, y=100)
+
+# Pikkuse textbox loomine(kg)
+pikkus_info = tk.Label(text="Palun sisestage oma kaal kilogrammides!")
+pikkus_info.pack(side="left")
+pikkus_info.place(x=50, y=175)
 
 # Pikkuse (Entry) loomine
 pikkus = Entry(master=win, font=("Comicsans", 23))
@@ -33,9 +43,12 @@ pikkus.place(x=50, y=200)
 # BMI nupp
 calculation_button = tk.Button(master=win, text="Vastus!", command=lambda: bmi_calc(kaal, pikkus, result_label)) 
 calculation_button.pack()
+calculation_button.place(x=50, y=300)
 
-result_label = tk.Label(text="")
-result_label.pack()
+# Lõpp vastus 
+result_label = tk.Label(text="", font=("Comicsans", 36))
+result_label.pack(side="top")
+result_label.place(x=425, y=145)
 
 # Excecutable
 win.mainloop()
