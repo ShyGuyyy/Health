532eca4d4108e8d8817fcf683aac3f79a0b1be16
ShyGuyyy <kaspupavel@gmail.com>
2023-03-27 11:25:02 +0300

    commit

--- main.py
+++ main.py
@@ -20,35 +20,49 @@ win.title("Health!")
 # Äppi geomeetriline suurus
 win.geometry("800x400")
 
+tabs = ttk.Notebook(win)
+
+tab1 = ttk.Frame(win)
+
+tabs.add(tab1, text='BMI')
+tabs.pack(expand=1, fill="both")
+
 # Kaalu textbox loomine(m)
-kaal_info = tk.Label(text="Palun sisestage oma pikkus meetrites!")
+kaal_info = ttk.Label(tab1, text="Palun sisestage oma pikkus meetrites!")
 kaal_info.pack(side="left")
 kaal_info.place(x=50, y=75)
 
 # Kaalu (Entry) loomine
-kaal = Entry(master=win, font=("Comicsans", 23))
+kaal = ttk.Entry(tab1, font=("Comicsans", 23))
 kaal.pack(side="left")
 kaal.place(x=50, y=100)
 
 # Pikkuse textbox loomine(kg)
-pikkus_info = tk.Label(text="Palun sisestage oma kaal kilogrammides!")
+pikkus_info = ttk.Label(tab1, text="Palun sisestage oma kaal kilogrammides!")
 pikkus_info.pack(side="left")
 pikkus_info.place(x=50, y=175)
 
 # Pikkuse (Entry) loomine
-pikkus = Entry(master=win, font=("Comicsans", 23))
+pikkus = ttk.Entry(tab1, font=("Comicsans", 23))
 pikkus.pack(side="left")
 pikkus.place(x=50, y=200)
 
 # BMI nupp
-calculation_button = tk.Button(master=win, text="Vastus!", command=lambda: bmi_calc(kaal, pikkus, result_label)) 
+calculation_button = ttk.Button(tab1, text="Vastus!", command=lambda: bmi_calc(kaal, pikkus, result_label)) 
 calculation_button.pack()
 calculation_button.place(x=50, y=300)
 
 # Lõpp vastus 
-result_label = tk.Label(text="", font=("Comicsans", 36))
+result_label = ttk.Label(tab1, text="", font=("Comicsans", 36))
 result_label.pack(side="top")
 result_label.place(x=425, y=145)
+
+# ______________________________
+
+tab2 = ttk.Frame(win)
+
+tabs.add(tab2, text='Kalorid')
+tabs.pack(expand=1, fill="both")
 
 # Excecutable
 win.mainloop()
